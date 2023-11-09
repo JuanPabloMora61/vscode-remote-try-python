d5540474de28975de2fc73f88bcf4985ca38cf3e
Juan Pablo Mora Del Castillo <120071610+JuanPabloMora61@users.noreply.github.com>
2023-11-09 04:06:09 +0000

    App desarrollada

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
+# write 'hello world' to the console
+print("hello world")
 
 from flask import Flask
 app = Flask(__name__)
@@ -9,3 +11,26 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+import random
+
+opciones = ["piedra", "papel", "tijeras"]
+
+while True:
+    eleccion_jugador = input("Elige piedra, papel o tijeras: ").lower()
+    if eleccion_jugador not in opciones:
+        print("Opción no válida. Inténtalo de nuevo.")
+        continue
+    eleccion_oponente = random.choice(opciones)
+    print("El oponente eligió " + eleccion_oponente)
+    if eleccion_jugador == eleccion_oponente:
+        print("Es un empate.")
+    elif (eleccion_jugador == "piedra" and eleccion_oponente == "tijeras") or \
+         (eleccion_jugador == "tijeras" and eleccion_oponente == "papel") or \
+         (eleccion_jugador == "papel" and eleccion_oponente == "piedra"):
+        print("¡Has ganado!")
+    else:
+        print("Has perdido.")
+    jugar_de_nuevo = input("¿Quieres jugar de nuevo? (s/n): ").lower()
+    if jugar_de_nuevo != "s":
+        break
